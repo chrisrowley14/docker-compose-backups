a505a36afd9aaa96a899ad5bac2d23f309578952
chrisrowley14 <chrisrowley14@users.noreply.github.com>
2021-02-23 18:56:10 +0000

    Update BaseSetup.sh

--- BaseSetup.sh
+++ BaseSetup.sh
@@ -20,20 +20,16 @@ sudo apt install zfsutils-linux
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 #sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt install docker-ce docker-ce-cli containerd.io -y
-sudo apt install docker-compose -y
+sudo apt install docker-ce docker-ce-cli containerd.io docker-compose -y
 sudo usermod -aG docker ${USER}
 
 #move docker data
 cp /lib/systemd/system/docker.service /lib/systemd/system/docker.service.bak
 nano /lib/systemd/system/docker.service
 #change line starting with ExecStart to ExecStart=/usr/bin/dockerd -g /DISK_1T/docker -H fd:// --containerd=/run/containerd/containerd.sock
-/etc/init.d/docker restart
 systemctl daemon-reload
 /etc/init.d/docker restart
 
 #Portainer
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
